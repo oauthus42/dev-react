41049bc33a04ddb3cbbdea3c9df6d929ce16ccd0
Oauthus42 <var.verdandi@gmail.com>
2023-03-08 23:00:08 +0300

    Остальные компоненты + роутинг

--- src/App.js
+++ src/App.js
@@ -5,18 +5,31 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Dialogs from './components/Dialogs/Dialogs';
 import Profile from './components/Profile/Profile';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import News from './components/News/News';
+import Settings from './components/Settings/Settings';
+import Music from './components/Music/Music';
 
 
 const App = () => {
   return (
-    <div className='app-wrapper'>
-      <Header />
-      <Navbar />
-      <div className='app-wrapper-content'>
-        <Dialogs />
+      <div className='app-wrapper'>
+        <Header />
+        <Navbar />
+        <div className='app-wrapper-content'>
+        <BrowserRouter>
+        <Routes>
+            <Route path="profiles" element={<Profile />} />
+            <Route path="dialogs" element={<Dialogs />} />
+            <Route path="news" element={<News />} />
+            <Route path="music" element={<Music />} />
+            <Route path='settings' element={<Settings />} />
+          </Routes>
+          </BrowserRouter>
+        </div>
       </div>
-      {/*<Profile />*/}
-    </div>);
+   
+  );
 }
 
 export default App;
---
+++ src/components/Music/Music.jsx
@@ -0,0 +1,10 @@
+import s from './Music.module.css';
+console.log(s);
+
+const Music = () => {
+    return (
+       <div> Music </div>
+    )
+}
+
+export default Music;

ecfe6765f4673160343685ff51588e8ec7e38984
Oauthus42 <var.verdandi@gmail.com>
2023-03-08 21:35:42 +0300

    DIaloges + рефакторинг

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
+import Dialogs from './components/Dialogs/Dialogs';
 import Profile from './components/Profile/Profile';
+
 
 const App = () => {
   return (
     <div className='app-wrapper'>
       <Header />
       <Navbar />
-      <Profile />
+      <div className='app-wrapper-content'>
+        <Dialogs />
+      </div>
+      {/*<Profile />*/}
     </div>);
 }
 
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import s from './Profile.module.css'
 
 const Profile = () => {
   return (
-    <div className={s.content}>
+    <div>
       <div>
         <img src='https://catherineasquithgallery.com/uploads/posts/2021-12/thumbs/1639732741_62-catherineasquithgallery-com-p-rozovii-fon-dlya-kanala-99.jpg'></img>
       </div>

7e2903804ea2a8bc96f6aff6dc1ef5271e8aaebf
Oauthus42 <var.verdandi@gmail.com>
2023-03-11 02:25:57 +0300

    исправление роутов, NavLink, css

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Dialogs from './components/Dialogs/Dialogs';
 import Profile from './components/Profile/Profile';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import News from './components/News/News';
 import Settings from './components/Settings/Settings';
 import Music from './components/Music/Music';
@@ -13,21 +13,23 @@ import Music from './components/Music/Music';
 
 const App = () => {
   return (
-      <div className='app-wrapper'>
-        <Header />
+    <div className='app-wrapper'>
+      <Header />
+      <Router>
         <Navbar />
         <div className='app-wrapper-content'>
-        <BrowserRouter>
-        <Routes>
-            <Route path="profiles" element={<Profile />} />
-            <Route path="dialogs" element={<Dialogs />} />
-            <Route path="news" element={<News />} />
-            <Route path="music" element={<Music />} />
-            <Route path='settings' element={<Settings />} />
-          </Routes>
-          </BrowserRouter>
+            <Routes>
+              <Route path="profiles" element={<Profile />} />
+              <Route path="dialogs" element={<Dialogs />} />
+              <Route path="news" element={<News />} />
+              <Route path="music" element={<Music />} />
+              <Route path='settings' element={<Settings />} />
+            </Routes>
         </div>
-      </div>
+      </Router>
+    
+        
+      </div >
    
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,24 @@
-import { scryRenderedDOMComponentsWithClass } from "react-dom/test-utils";
 import s from './Navbar.module.css';
+import { NavLink } from 'react-router-dom';
 console.log(s);
 
 const Navbar = () => {
     return (
         <nav className={s.nav}>
             <div className={`${s.item} ${s.active}`}>
-                <a href="/profiles">Profile</a>
+                <NavLink to="/profiles">Profile</NavLink>
             </div>
             <div className={s.item}>
-                <a href="/dialogs">Messages</a>
+                <NavLink to="/dialogs">Messages</NavLink>
             </div>
             <div className={s.item}>
-                <a href="/news">News</a>
+                <NavLink to="/news">News</NavLink>
             </div>
             <div className={s.item}>
-                <a href="/music">Music</a>
+                <NavLink to="/music">Music</NavLink>
             </div>
             <div className={s.item}>
-                <a href="/settings">Settings</a>
+                <NavLink to="/settings">Settings</NavLink>
             </div>
         </nav>
     )

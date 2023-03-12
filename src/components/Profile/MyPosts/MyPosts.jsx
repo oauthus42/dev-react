168eed6e89df1ca61a7f8db5660518866235566a
Oauthus42 <var.verdandi@gmail.com>
2023-03-12 18:51:06 +0300

    map данных

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -5,7 +5,7 @@ import Post from './Post/Post';
 let postsData = [
   {id: '1', message: 'Hi, how are you?', likesCount: '12'},
   {id: '2', message: "It's my first post", likesCount: '10'},
-  {id: '3', message: 'Marina Z', likesCount: 'больше всех'}
+  {id: '3', message: 'Marina Z', likesCount: 'лучше всех'}
 ]
 
 const MyPosts = () => {

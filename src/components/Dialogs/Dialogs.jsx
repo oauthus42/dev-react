a03b069d409c675698cb39899bb0db85492ef1fb
Oauthus42 <var.verdandi@gmail.com>
2023-03-13 01:31:24 +0300

    разделение ui и bll

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,20 +1,8 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import s from './Dialogs.module.css';
-
-
-const DialogItem = (props) => {
-  let path = "/dialogs/" + props.id;
-  return (
-    <div className={s.dialog + ' ' + s.active}>
-      <NavLink to={path}>{props.name}</NavLink>
-    </div>
-  )
-}
-
-const Message = (props) => {
-  return <div className={s.message}>{props.message}</div>
-}
+import DialogItem from './DialogItem/DialogItem'
+import Message from './Message/Message'
 
 let dialogData = [
   { id: '1', name: 'Polina' },
